c8a39225b46af969e9d0e8c4c81c78600fe7ec72
Satyam Chauhan <satyam.chauhan@gmail.com>
2019-06-21 14:08:10 +0530

    Initial Commit

---
+++ concerntarationTests/concerntarationTests.swift
@@ -0,0 +1,34 @@
+//
+//  concerntarationTests.swift
+//  concerntarationTests
+//
+//  Created by Zomato on 21/06/19.
+//  Copyright © 2019 Zomato. All rights reserved.
+//
+
+import XCTest
+@testable import concerntaration
+
+class concerntarationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
